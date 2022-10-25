1b3abbdc5dadc53a90807b0a605af6f35944d3dd
Mouad El Asri <elasri.mouad.2002@gmail.com>
2022-10-25 22:07:42 +0100

    third commit

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: moel-asr <moel-asr@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 15:36:36 by moel-asr          #+#    #+#             */
-/*   Updated: 2022/10/24 09:39:48 by moel-asr         ###   ########.fr       */
+/*   Updated: 2022/10/25 19:26:18 by moel-asr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,17 @@ int	ft_check(char c, va_list ap)
 
 	size = 0;
 	if (c == 'c')
-				size += ft_putchar(va_arg(ap, int));
+		size += ft_putchar(va_arg(ap, int));
 	else if (c == 's')
 		size += ft_putstr(va_arg(ap, char *));
 	else if (c == 'd' || c == 'i')
 		size += ft_putnbr(va_arg(ap, int));
 	else if (c == 'x')
-		size += ft_puthex(va_arg(ap, int), "0123456789abcdef");
+		size += ft_puthex(va_arg(ap, unsigned int), "0123456789abcdef");
 	else if (c == 'X')
-		size += ft_puthex(va_arg(ap, int), "0123456789ABCDEF");
+		size += ft_puthex(va_arg(ap, unsigned int), "0123456789ABCDEF");
 	else if (c == 'u')
-		size += ft_put_unsigned_int(va_arg(ap, int));
+		size += ft_put_unsigned_int(va_arg(ap, unsigned int));
 	else if (c == 'p')
 	{
 		size += ft_putstr("0x");
